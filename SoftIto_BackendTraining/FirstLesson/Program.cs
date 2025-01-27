87a6cf782109b4857145937e12813692faf5b5d1
Gizem Anbarcı <gzmanbrc@outlook.com>
2025-01-27 23:02:38 +0300

    Started the Online Training

---
+++ SoftIto_BackendTraining/FirstLesson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// WHY we use using ?  
+// The "using" statement simplifies object management and automatically releases resources, preventing memory leaks.
+//"Using" bloğunda tanımlanan nesneler, bloğun sonunda otomatik olarak Dispose() yöntemiyle temizlenir
+
+namespace FirstLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+
+            // Variables
+
+            //Variable --> Storage location that holds a value, temporarily holding information in the computer's memory.
+
+            // principles
+            // camelCase --> sumNumber
+            // UpperCase --> SumNumber
+
+
+
+        }
+    }
+}
